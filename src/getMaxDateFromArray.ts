5565dcc204f80a2cc1a8e9356483def2afeef2f6
Whiskey Actual <actualwhiskey@gmail.com>
2023-10-13 10:29:22 -0400

    update getMaxDateFromArray()

--- src/getMaxDateFromArray.ts
+++ src/getMaxDateFromArray.ts
@@ -1,10 +1,10 @@
+import { CleanedDate } from './CleanedDate';
 import { minimumJsonDate } from './minimumJsonDate'
 
 export const getMaxDateFromArray = function(dates:Date[]):Date|undefined{
     let output:Date|undefined = undefined
     try {
-        output = new Date(Math.max(...dates.map( d=> d ? d.getTime() : minimumJsonDate.getTime())));
-        if(output===minimumJsonDate) { output = undefined }
+        output = CleanedDate(new Date(Math.max(...dates.map( d=> d ? d.getTime() : minimumJsonDate.getTime()))));
     } catch(err) {
         throw `getMaxDateFromArray: ${err}`
     }
