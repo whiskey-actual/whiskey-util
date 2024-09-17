e05811783d3edb0ef9044856280d5cf2ef6cf640
Whiskey Actual <actualwhiskey@gmail.com>
2024-09-17 09:01:40 -0400

    tweak

--- src/CleanedDate.ts
+++ src/CleanedDate.ts
@@ -1,14 +1,16 @@
+import { isDate } from "util/types"
 import { minimumJsonDate } from "./minimumJsonDate"
 
 export const CleanedDate= function(objectToClean:any):Date|undefined {
     let output:Date|undefined=undefined
 
     try {
-
+      
       if(objectToClean && objectToClean!==null) {
         let dateObject:Date = new Date(objectToClean)
-        if(dateObject instanceof Date && !isNaN(dateObject.valueOf()) && dateObject>minimumJsonDate) { output = dateObject }
+        if(!isNaN(dateObject.getTime()) && isDate(dateObject) && !isNaN(dateObject.valueOf()) && dateObject>minimumJsonDate) { output = dateObject }
       }
+
     } catch(err) {
       throw(`${arguments.callee.toString()} : [${objectToClean}] : ${err}`)
     }
